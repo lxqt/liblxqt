a2d6337d0644b0a24098f4db8183c759302f53b2
attuska <attus@enterpriseforever.com>
2015-05-21 15:31:08 +0200

    Hungarian translation update

--- translations/liblxqt_hu.ts
+++ translations/liblxqt_hu.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="hu">
+<TS version="2.1" language="hu">
 <context>
     <name>AddPluginDialog</name>
     <message>
         <location filename="../addplugindialog/lxqtaddplugindialog.ui" line="22"/>
         <source>Search:</source>
-        <translation type="unfinished"></translation>
+        <translation>Keres:</translation>
     </message>
     <message>
         <location filename="../addplugindialog/lxqtaddplugindialog.ui" line="52"/>
         <source>Add Widget</source>
-        <translation type="unfinished"></translation>
+        <translation>Bigyó hozzáadás</translation>
     </message>
     <message>
         <location filename="../addplugindialog/lxqtaddplugindialog.ui" line="59"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezár</translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Párbeszédablak</translation>
     </message>
 </context>
 <context>
@@ -32,61 +32,61 @@
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="86"/>
         <source>Version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzió: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="99"/>
         <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Qt alapú könnyen használható, fejlett és gyors asztalkezelő.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="101"/>
         <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Az LXQt a &lt;a %1&gt;Razor-qt&lt;/a&gt;-és annak fejlesztői nélkül nem létezne.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="103"/>
         <source>Copyright: %1-%2 %3</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Jogok: %1-%2 %3</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="105"/>
         <source>Homepage: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Honlap: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="107"/>
         <source>License: %1</source>
         <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Licensz: %1</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="117"/>
         <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Az LXQt fejlesztői&lt;a %1&gt;LXQt Team és a társasága&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="119"/>
         <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
         <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Ha érdekli a fejlesztés. akkor &lt;a %1&gt;csatlakozhat hozzánk&lt;/a&gt;.</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="137"/>
         <source>Special thanks to:</source>
         <comment>About dialog, &apos;Thanks&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Külön köszönet:</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.cpp" line="144"/>
         <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world.</source>
         <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Az LXQT sok nyelvre le van fordítva, a világban működő fordítóknak köszönhetőn.</translation>
     </message>
 </context>
 <context>
@@ -94,7 +94,7 @@
     <message>
         <location filename="../addplugindialog/lxqtaddplugindialog.cpp" line="199"/>
         <source>(%1 active)</source>
-        <translation type="unfinished"></translation>
+        <translation>(%1 aktív)</translation>
     </message>
 </context>
 <context>
@@ -102,12 +102,12 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="70"/>
         <source>LxQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Energiakezelő hiba</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="70"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Elaltatás sikertelen</translation>
     </message>
 </context>
 <context>
@@ -115,7 +115,7 @@
     <message>
         <location filename="../lxqtnotification.cpp" line="152"/>
         <source>Notifications Fallback</source>
-        <translation type="unfinished"></translation>
+        <translation>Vészhitelesítés</translation>
     </message>
 </context>
 <context>
@@ -123,93 +123,93 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="113"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Elaltatás</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="120"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Készültség</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="127"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Újraindítás</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="134"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Kikapcsolás</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="141"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Kijelentkezés</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="153"/>
         <source>LxQt Session Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Az LXQT készenléte</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="154"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Készenléti állapotba akarja helyezni a gépet?&lt;p&gt;A kis energiaszükségletű állapotban feszültségkimaradáskor az újraélesztés nem biztos.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="163"/>
         <source>LxQt Session Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Az LXQT elaltatása</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="164"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mélykészenléti állapotba akarja helyezni a gépet?&lt;p&gt;A kis energiaszükségletű alvó állapotban feszültségkimaradáskor az újraélesztés nem biztos.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="173"/>
         <source>LxQt Session Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>LxQt újraindítás</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="174"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Újra akarja indítani a gépet? Minden mentetlen munka elvész...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="183"/>
         <source>LxQt Session Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>LxQt kikapcsolás</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="184"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ki akarja kapcsolni a gépet? Minden mentetlen munka elvész...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="193"/>
         <source>LxQt Session Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>LxQt kijelentkezés</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="194"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Kijelentkezik? Minden mentetlen munka elvész...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <location filename="../lxqtpowermanager.cpp" line="207"/>
         <source>LxQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>LxQt energiakezelő hiba</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mélykészenléti állapot sikertelen</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="207"/>
         <source>Suspend failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Készenléti állapot sikertelen</translation>
     </message>
 </context>
 <context>
@@ -217,7 +217,7 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="52"/>
         <source>Lock Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Képernyőzár</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="71"/>
@@ -228,42 +228,42 @@
         <location filename="../lxqtscreensaver.cpp" line="115"/>
         <location filename="../lxqtscreensaver.cpp" line="124"/>
         <source>Screen Saver Activation Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Képvédő indítási hiba</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="72"/>
         <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Az xdg-képernyővédő összeomlott.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="80"/>
         <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Az xdg-képernyővédő nincs jól telepítve.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="88"/>
         <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Az xdg-képernyővédő indíthatatlan.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="100"/>
         <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Az xdg-képernyővédő indító paramétere hibás.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="108"/>
         <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Az xdg-képernyővédő telepített és futtatható?</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="116"/>
         <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Az &apos;activate&apos; sikertelen. Az xdg-képernyővédő telepített és futtatható?</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="125"/>
         <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver=%1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hiba a képernyővédő indításakor.Ismeretlen hiba - az xdg-képernyővédő ismeretlen hibakóddal kilépett=%1.</translation>
     </message>
 </context>
 <context>
@@ -272,19 +272,19 @@
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="91"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="141"/>
         <source>Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Energiakezelő hiba</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="92"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="142"/>
         <source>QDBusInterface is invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>A QDBusInterface érvénytelen</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="106"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="156"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation type="unfinished"></translation>
+        <translation>Energiakezelő hiba (D-BUS hívás)</translation>
     </message>
 </context>
 <context>
@@ -292,42 +292,42 @@
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="14"/>
         <source> About LXQt</source>
-        <translation type="unfinished"></translation>
+        <translation>Az LXQT -ről</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="66"/>
         <source>About</source>
         <comment>About dialog, Tab title </comment>
-        <translation type="unfinished"></translation>
+        <translation>Rólunk</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="83"/>
         <source>Authors</source>
         <comment>About dialog, Tab title</comment>
-        <translation type="unfinished"></translation>
+        <translation>Szerzők</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="100"/>
         <source>Thanks</source>
         <comment>About dialog, Tab title</comment>
-        <translation type="unfinished"></translation>
+        <translation>Köszönet</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="117"/>
         <source>Translations</source>
         <comment>About dialog, Tab title</comment>
-        <translation type="unfinished"></translation>
+        <translation>Fordítások</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="134"/>
         <source>Technical Info</source>
         <comment>About dialog, Tab title</comment>
-        <translation type="unfinished"></translation>
+        <translation>Technikai háttér</translation>
     </message>
     <message>
         <location filename="../aboutdialog/lxqtaboutdialog.ui" line="165"/>
         <source>Copy to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Vágólapra másol</translation>
     </message>
 </context>
 </TS>
