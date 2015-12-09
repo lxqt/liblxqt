c33265c2a14acb2d37ce7f9252643a0526fa6e06
Igor <f2404@yandex.ru>
2015-12-09 13:28:03 +0300

    Updated Russian translation Removed ru_RU file

--- translations/liblxqt_ru.ts
+++ translations/liblxqt_ru.ts
@@ -2,85 +2,11 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru">
 <context>
-    <name>AddPluginDialog</name>
-    <message>
-        <source>Search:</source>
-        <translation type="vanished">Поиск:</translation>
-    </message>
-    <message>
-        <source>Add Widget</source>
-        <translation type="vanished">Добавить виджет</translation>
-    </message>
-    <message>
-        <source>Close</source>
-        <translation type="vanished">Закрыть</translation>
-    </message>
-</context>
-<context>
     <name>ConfigDialog</name>
     <message>
         <location filename="../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>LXQt::AboutDialogPrivate</name>
-    <message>
-        <source>Version: %1</source>
-        <translation type="vanished">Версия: %1</translation>
-    </message>
-    <message>
-        <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
-        <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="vanished">Передовая, быстрая, и простая в использовании среда рабочего стола на основе технологий Qt.</translation>
-    </message>
-    <message>
-        <source>LXQt would not have been possible without the &lt;a %1&gt;Razor-qt&lt;/a&gt; project and its many contributors.</source>
-        <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="vanished">LXQT не могло бы существовать без проекта &lt;a %1&gt;Razor-qt&lt;/a&gt; и многих его участников.</translation>
-    </message>
-    <message>
-        <source>Copyright: %1-%2 %3</source>
-        <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="vanished">Авторские права: %1-%2 %3</translation>
-    </message>
-    <message>
-        <source>Homepage: %1</source>
-        <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="vanished">Домашняя страница: %1</translation>
-    </message>
-    <message>
-        <source>License: %1</source>
-        <comment>About dialog, &apos;About&apos; tab text</comment>
-        <translation type="vanished">Лицензия: %1</translation>
-    </message>
-    <message>
-        <source>LXQt is developed by the &lt;a %1&gt;LXQt Team and contributors&lt;/a&gt;.</source>
-        <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation type="vanished">LXQT разработан &lt;a %1&gt;командой LXQt и прочими участниками&lt;/a&gt;.</translation>
-    </message>
-    <message>
-        <source>If you are interested in working with our development team, &lt;a %1&gt;join us&lt;/a&gt;.</source>
-        <comment>About dialog, &apos;Authors&apos; tab text</comment>
-        <translation type="vanished">Если вы заинтересованы работой с нашей командой разработки, &lt;a %1&gt;присоединяйтесь к нам&lt;/a&gt;.</translation>
-    </message>
-    <message>
-        <source>Special thanks to:</source>
-        <comment>About dialog, &apos;Thanks&apos; tab text</comment>
-        <translation type="vanished">Особая благодарность:</translation>
-    </message>
-    <message>
-        <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world.</source>
-        <comment>About dialog, &apos;Translations&apos; tab text</comment>
-        <translation type="vanished">LXQT переведён на множество языков благодаря работе команд переводчиков по всему миру.</translation>
-    </message>
-</context>
-<context>
-    <name>LXQt::AddPluginDialog</name>
-    <message>
-        <source>(%1 active)</source>
-        <translation type="vanished">(%1 активный)</translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -139,7 +65,7 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="153"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation>Перевести компьютер в ждущий режим?&lt;p&gt;Переводит компьютер в режим низкого энергопотрбления. Состояние системы будет потеряно при отключении питания.</translation>
+        <translation>Перевести компьютер в ждущий режим?&lt;p&gt;Переводит компьютер в режим низкого энергопотрбления. В случае отключения питания состояние системы будет потеряно.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="162"/>
@@ -159,17 +85,17 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="173"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation>Перезагрузить компьютер? Все несохранённые изменения в документах будут потеряны…</translation>
+        <translation>Перезагрузить компьютер? Все несохранённые изменения в документах будут потеряны...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="182"/>
         <source>LXQt Session Shutdown</source>
-        <translation>Завершение сеанса LXQt</translation>
+        <translation>Выключение сеанса LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="183"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation>Выключить компьютер? Все несохранённые изменения в документах будут потеряны…</translation>
+        <translation>Выключить компьютер? Все несохранённые изменения в документах будут потеряны...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="192"/>
@@ -179,7 +105,7 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="193"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation>Завершить сеанс? Все несохранённые изменения в документах будут потеряны…</translation>
+        <translation>Завершить сеанс? Все несохранённые изменения в документах будут потеряны...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="201"/>
@@ -208,7 +134,7 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="77"/>
         <source>Screen Saver Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка хранителя экрана</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="83"/>
@@ -220,19 +146,7 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="97"/>
         <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver: %1.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
-        <translation type="vanished">При запуске хранителя экрана произошла ошибка. Невозможно запустить xdg-screensaver по причине его сбоя.</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
-        <translation type="vanished">При запуске хранителя экрана произошла ошибка. Xdg-screensaver установлен некорректно.</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
-        <translation type="vanished">При запуске хранителя экрана произошла ошибка. Xdg-screensaver не может быть запущен.</translation>
+        <translation>При запуске хранителя экрана произошла ошибка. Неизвестная ошибка - недокументированный код возврата xdg-screensaver: %1.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="78"/>
@@ -247,11 +161,7 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="90"/>
         <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation>При запуске заставки произошла ошибка. Невозможно выполнить действие «запустить». Проверьте, чтобы xscreensaver был установлен и запущен.</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver=%1.</source>
-        <translation type="vanished">При запуске заставки произвошла ошибка. Неизвестная ошибка - недокументированное значение возвращено из xdg-screensaver=%1.</translation>
+        <translation>При запуске хранителя экрана произошла ошибка. Невозможно выполнить действие «запустить». Убедитесь, что xscreensaver установлен и запущен.</translation>
     </message>
 </context>
 <context>
@@ -260,7 +170,7 @@
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="91"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="141"/>
         <source>Power Manager Error</source>
-        <translation>Ошибка модуля управления питанием</translation>
+        <translation>Ошибка управления энергопотреблением</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="92"/>
@@ -273,42 +183,6 @@
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="156"/>
         <source>Power Manager Error (D-BUS call)</source>
         <translation>Ошибка управления энергопотреблением (вызов D-BUS)</translation>
-    </message>
-</context>
-<context>
-    <name>about</name>
-    <message>
-        <source> About LXQt</source>
-        <translation type="vanished"> О LXQt</translation>
-    </message>
-    <message>
-        <source>About</source>
-        <comment>About dialog, Tab title </comment>
-        <translation type="vanished">О программе</translation>
-    </message>
-    <message>
-        <source>Authors</source>
-        <comment>About dialog, Tab title</comment>
-        <translation type="vanished">Авторы</translation>
-    </message>
-    <message>
-        <source>Thanks</source>
-        <comment>About dialog, Tab title</comment>
-        <translation type="vanished">Благодарности</translation>
-    </message>
-    <message>
-        <source>Translations</source>
-        <comment>About dialog, Tab title</comment>
-        <translation type="vanished">Переводы</translation>
-    </message>
-    <message>
-        <source>Technical Info</source>
-        <comment>About dialog, Tab title</comment>
-        <translation type="vanished">Техническая информация</translation>
-    </message>
-    <message>
-        <source>Copy to clipboard</source>
-        <translation type="vanished">Копировать в буфер обмена</translation>
     </message>
 </context>
 </TS>
