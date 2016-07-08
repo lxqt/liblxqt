bb220ebe24d3cdf89b3c8d9f4bb0f777f97f30ab
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

--- liblxqt_ar.ts
+++ liblxqt_ar.ts
@@ -2,41 +2,11 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ar">
 <context>
-    <name>AddPluginDialog</name>
-    <message>
-        <source>Add plugins</source>
-        <translation type="obsolete">ضمُّ إضافاتٍ برمجيَّة</translation>
-    </message>
-    <message>
-        <source>Search:</source>
-        <translation type="vanished">ابحث:</translation>
-    </message>
-    <message>
-        <source>Add plugin</source>
-        <translation type="obsolete">ضمُّ إضافةٍ برمجيَّة</translation>
-    </message>
-    <message>
-        <source>Close</source>
-        <translation type="vanished">إغلاق</translation>
-    </message>
-</context>
-<context>
     <name>ConfigDialog</name>
     <message>
         <location filename="../../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation>حوار</translation>
-    </message>
-</context>
-<context>
-    <name>LXQt::AboutDialogPrivate</name>
-    <message>
-        <source>Yes</source>
-        <translation type="obsolete">نعم</translation>
-    </message>
-    <message>
-        <source>No</source>
-        <translation type="obsolete">ﻻ</translation>
+        <translation>حواريّ</translation>
     </message>
 </context>
 <context>
@@ -44,12 +14,12 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="70"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished">خطأٌ في إدارة الطَّاقة لنظام ريزر</translation>
+        <translation>خطأ في مدير لكسكيوت للطّاقة</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="70"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished">فشل الانتقال إلى حالة السُّبات</translation>
+        <translation>فشل الإسبات.</translation>
     </message>
 </context>
 <context>
@@ -57,7 +27,7 @@
     <message>
         <location filename="../../lxqtnotification.cpp" line="152"/>
         <source>Notifications Fallback</source>
-        <translation>ارتداد اﻹشعارات</translation>
+        <translation>احتياطيّ الإخطارات</translation>
     </message>
 </context>
 <context>
@@ -65,93 +35,93 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="112"/>
         <source>Hibernate</source>
-        <translation>سبات</translation>
+        <translation>أسبت</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="119"/>
         <source>Suspend</source>
-        <translation>تعليق</translation>
+        <translation>علّق</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="126"/>
         <source>Reboot</source>
-        <translation>إعادة إقلاع</translation>
+        <translation>أعد الإقلاع</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="133"/>
         <source>Shutdown</source>
-        <translation>إطفاء</translation>
+        <translation>أطفئ</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="140"/>
         <source>Logout</source>
-        <translation>خروج</translation>
+        <translation>اخرج</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="152"/>
         <source>LXQt Session Suspend</source>
-        <translation>وضع جلسة نظام ريزر في حالة تعليق</translation>
+        <translation>تعليق جلسة لكسكيوت</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="153"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation>هل ترغب حقَّاً بتعليق الحاسب؟&lt;p&gt;تعليق الحاسب في وضعيَّة طاقةٍ أخفّ ولا يتم حفظ حالة النِّظام في حال انقطاع التَّيَّار.</translation>
+        <translation>أمتأكّد من تعليق الحاسوب؟&lt;p&gt;يعلّق الحاسوب في وضع طاقة منخفض. لن تُحفظ حالة النّظام إن فُقدت الطّاقة.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="162"/>
         <source>LXQt Session Hibernate</source>
-        <translation>وضع جلسة نظام ريزر في حالة سبات</translation>
+        <translation>إسبات جلسة لكسكيوت</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="163"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation>هل ترغب حقَّاً بوضع الحاسب في حالة سبات؟&lt;p&gt;يتم وضع الحاسب في حالة سباتٍ مع وضعيَّة طاقةٍ أخفّ ويتم حفظ حالة النِّظام في حال انقطاع التَّيَّار.</translation>
+        <translation>أمتأكّد من إسبات الحاسوب؟&lt;p&gt;يسبت الحاسوب في وضع طاقة منخفض. تُحفظ حالة النّظام إن فُقدت الطّاقة.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="172"/>
         <source>LXQt Session Reboot</source>
-        <translation>إعادة إقلاع جلسة نظام ريزر</translation>
+        <translation>إعادة إقلاع جلسة لكسكيوت</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="173"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation>هل ترغب حقَّاً بإعادة تشغيل الحاسب؟ سيتم إضاعة كافَّة اﻷعمال التي لم يتمَُ حفظها..</translation>
+        <translation>أمتأكّد من إعادة إقلاع الحاسوب؟ ستفقد كلّ عملك غير المحفوظ...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="182"/>
         <source>LXQt Session Shutdown</source>
-        <translation>إطفاء نظام ريزر</translation>
+        <translation>إطفاء جلسة لكسكيوت</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="183"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation>هل ترغب حقَّاً بإطفاء الحاسب؟  سيتم إضاعة كافَّة اﻷعمال التي لم يتمَُ حفظها..</translation>
+        <translation>أمتأكّد من إطفاء الحاسوب؟ ستفقد كلّ عملك غير المحفوظ...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="192"/>
         <source>LXQt Session Logout</source>
-        <translation>الخروج من جلسة نظام ريزر</translation>
+        <translation>الخروج من جلسة لكسكيوت</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="193"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation>هل ترغب بالخروج حقَّاً؟  سيتم إضاعة كافَّة اﻷعمال التي لم يتمَُ حفظها..</translation>
+        <translation>أمتأكّد من الخروج؟ ستفقد كلّ عملك غير المحفوظ...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="201"/>
         <location filename="../../lxqtpowermanager.cpp" line="206"/>
         <source>LXQt Power Manager Error</source>
-        <translation>خطأٌ في إدارة الطَّاقة لنظام ريزر</translation>
+        <translation>خطأ في مدير لكسكيوت للطّاقة</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="201"/>
         <source>Hibernate failed.</source>
-        <translation>فشل الانتقال إلى حالة السُّبات</translation>
+        <translation>فشل الإسبات.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="206"/>
         <source>Suspend failed.</source>
-        <translation>فشل الانتقال إلى حالة تعليق الحاسب</translation>
+        <translation>فشل التّعليق.</translation>
     </message>
 </context>
 <context>
@@ -159,66 +129,39 @@
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="51"/>
         <source>Lock Screen</source>
-        <translation>إقفال الشَّاشة</translation>
+        <translation>اقفل الشّاشة</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="77"/>
         <source>Screen Saver Error</source>
-        <translation type="unfinished"></translation>
+        <translation>خطأ في حافظ الشّاشة</translation>
+    </message>
+    <message>
+        <location filename="../../lxqtscreensaver.cpp" line="78"/>
+        <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
+        <translation>حدث خطأ في تشغيل حافظ الشّاشة. الخطأ صياغيّ في معاملات حافظ xdg للشّاشة.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="83"/>
         <location filename="../../lxqtscreensaver.cpp" line="89"/>
         <location filename="../../lxqtscreensaver.cpp" line="96"/>
         <source>Screen Saver Activation Error</source>
-        <translation>خطأٌ في تشغيل حافظ الشَّاشة</translation>
-    </message>
-    <message>
-        <location filename="../../lxqtscreensaver.cpp" line="97"/>
-        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver: %1.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
-        <translation type="vanished">حدث خطأٌ في تشغيل حافظ الشَّاشة! ﻻ يمكن تشغيل حافظ شاشة سطح المكتب الرُّسوميِّ نظراً لانهياره..!</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
-        <translation type="vanished">حدث خطأٌ في تشغيل حافظ الشَّاشة! لم يتمُّ تثبيت حافظ شاشة سطح المكتب الرُّسوميِّ بالشَّكل الصَّحيح..!</translation>
-    </message>
-    <message>
-        <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
-        <translation type="vanished">حدث خطأٌ في تشغيل حافظ الشَّاشة! ﻻ يمكن تشغيل حافظ شاشة سطح المكتب الرُّسوميِّ..!</translation>
-    </message>
-    <message>
-        <location filename="../../lxqtscreensaver.cpp" line="78"/>
-        <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
-        <translation>حدث خطأٌ في تشغيل حافظ الشَّاشة! هناك خطأٌ في رموز ضبط تعريف حافظ شاشة سطح المكتب الرُّسوميِّ</translation>
+        <translation>خطأ في تفعيل حافظ الشّاشة</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="84"/>
         <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
-        <translation>حدث خطأٌ في تشغيل حافظ الشَّاشة! يرجى التَّأكد من أن حافظ شاشة سطح المكتب الرُّسوميِّ مثبَّتٌ ويعمل..</translation>
+        <translation>حدث خطأ في تشغيل حافظ الشّاشة. تأكّد أن xscreensaver مثبّت ويعمل.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="90"/>
         <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation>حدث خطأٌ في تشغيل حافظ الشَّاشة! فشلٌ في أمر التَّفعيل! يرجى التَّأكد من أن حافظ شاشة سطح المكتب الرُّسوميِّ مثبَّتٌ ويعمل..</translation>
+        <translation>حدث خطأ في تشغيل حافظ الشّاشة. فشل الإجراء &apos;activate&apos;. تأكّد أن xscreensaver مثبّت ويعمل.</translation>
     </message>
     <message>
-        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver=%1.</source>
-        <translation type="vanished">حصل خطأٌ عند تشغيل حافظ الضَّاضة. خطأٌ غير معروفٍ - قيمةٌ جوابيَّةٌ غير موثَّقةٍ من حافظ شاشة إكس دي جي=%1.</translation>
-    </message>
-</context>
-<context>
-    <name>LXQt::ShortcutButton</name>
-    <message>
-        <source>Clear</source>
-        <translation type="obsolete">مسح</translation>
-    </message>
-    <message>
-        <source>None</source>
-        <translation type="obsolete">ﻻشيء</translation>
+        <location filename="../../lxqtscreensaver.cpp" line="97"/>
+        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver: %1.</source>
+        <translation>حدث خطأ في تشغيل حافظ الشّاشة. خطأ مجهول - أُرجعت قيمة غير موثّقة من xdg-screensaver: %1.</translation>
     </message>
 </context>
 <context>
@@ -227,36 +170,19 @@
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="91"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="141"/>
         <source>Power Manager Error</source>
-        <translation>خطأ في مدير الطَّاقة</translation>
+        <translation>خطأ في مدير الطّاقة</translation>
     </message>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="92"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="142"/>
         <source>QDBusInterface is invalid</source>
-        <translation>واجهة اتصال كيوت غير صالحة</translation>
+        <translation>‏QDBusInterface غير صالح</translation>
     </message>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="106"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="156"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation>خطأ في مدير الطَّاقة (نداء من خطِّ التَّواصل)</translation>
-    </message>
-</context>
-<context>
-    <name>about</name>
-    <message>
-        <source> About LXQt</source>
-        <translation type="vanished">نبذةٌ عن نظام ريزر</translation>
-    </message>
-    <message>
-        <source>About</source>
-        <comment>About dialog, Tab title </comment>
-        <translation type="vanished">تبذةٌ عن</translation>
-    </message>
-    <message>
-        <source>Technical Info</source>
-        <comment>About dialog, Tab title</comment>
-        <translation type="vanished">معلوماتٌ تقنيَّةٌ</translation>
+        <translation>خطأ في مدير الطّاقة (نداء D-BUS)</translation>
     </message>
 </context>
 </TS>
