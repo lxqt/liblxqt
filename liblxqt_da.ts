b10709a9a5627800588e52c68441832badd31c5d
scootergrisen <scootergrisen@gmail.com>
2016-12-13 02:29:38 +0100

    First attempt to change all danish translation files from using i mix of "da" and "da_DK" and both, to only using "da"

--- liblxqt_da.ts
+++ liblxqt_da.ts
@@ -2,11 +2,131 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="da">
 <context>
+    <name>AddPluginDialog</name>
+    <message>
+        <source>Add plugins</source>
+        <translation type="obsolete">Tilføj plugins</translation>
+    </message>
+    <message>
+        <source>Search:</source>
+        <translation type="vanished">Søg:</translation>
+    </message>
+    <message>
+        <source>Add plugin</source>
+        <translation type="obsolete">Tilføj plugin</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="vanished">Afslut</translation>
+    </message>
+</context>
+<context>
     <name>ConfigDialog</name>
     <message>
         <location filename="../../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::AboutDialogPrivate</name>
+    <message>
+        <source>Version: %1</source>
+        <translation type="vanished">Version: %1</translation>
+    </message>
+    <message>
+        <source>Advanced, easy-to-use, and fast desktop environment based on Qt technologies.</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation type="vanished">Avanceret, nem at bruge, hurtigt skrivebordsmiljø baseret på Qt teknologier.</translation>
+    </message>
+    <message>
+        <source>Copyright: %1-%2 %3</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation type="vanished">Copyright: %1-%2 %3</translation>
+    </message>
+    <message>
+        <source>Homepage: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation type="vanished">Hjemmeside: %1</translation>
+    </message>
+    <message>
+        <source>License: %1</source>
+        <comment>About dialog, &apos;About&apos; tab text</comment>
+        <translation type="vanished">Licens: %1</translation>
+    </message>
+    <message>
+        <source>LXQt is translated into many languages thanks to the work of the translation teams all over the world.</source>
+        <comment>About dialog, &apos;Translations&apos; tab text</comment>
+        <translation type="vanished">LXQt er oversat til mange sprog takket være oversættere fra hele verden.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="obsolete">Ja</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="obsolete">Nej</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;LXQt Desktop Toolbox - Technical Info&lt;/b&gt;</source>
+        <translation type="obsolete">&lt;b&gt;LXQt Desktop Værktøjskasse - Teknisk Information&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation type="obsolete">Version</translation>
+    </message>
+    <message>
+        <source>Qt</source>
+        <translation type="obsolete">Qt</translation>
+    </message>
+    <message>
+        <source>Debug Build</source>
+        <translation type="obsolete">Fejlsøgningsversion</translation>
+    </message>
+    <message>
+        <source>System Configuration</source>
+        <translation type="obsolete">Systemindstillinger</translation>
+    </message>
+    <message>
+        <source>Share Directory</source>
+        <translation type="obsolete">Delt Katalog</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <comment>About dialog, Tab title</comment>
+        <translation type="obsolete">Oversættelser</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;User Directories&lt;/b&gt;</source>
+        <translation type="obsolete">&lt;b&gt;Bruger Biblioteker&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Xdg Data Home</source>
+        <translation type="obsolete">Xdg Brugerdata Katalog</translation>
+    </message>
+    <message>
+        <source>Xdg Config Home</source>
+        <translation type="obsolete">Xdg Konfigurationsdata Katalog</translation>
+    </message>
+    <message>
+        <source>Xdg Data Dirs</source>
+        <translation type="obsolete">Xdg Data Kataloger</translation>
+    </message>
+    <message>
+        <source>Xdg Cache Home</source>
+        <translation type="obsolete">Xdg Cache Katalog</translation>
+    </message>
+    <message>
+        <source>Xdg Runtime Home</source>
+        <translation type="obsolete">Xdg udførselskatalog</translation>
+    </message>
+    <message>
+        <source>Xdg Autostart Dirs</source>
+        <translation type="obsolete">Xdg Autostart Katalog</translation>
+    </message>
+    <message>
+        <source>Xdg Autostart Home</source>
+        <translation type="obsolete">Xdg Autostart Hjemmekatalog</translation>
     </message>
 </context>
 <context>
@@ -14,12 +134,12 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="70"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">LXQt Strømstyringsfejl</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="70"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Dvaletilstand mislykkedes.</translation>
     </message>
 </context>
 <context>
@@ -27,7 +147,7 @@
     <message>
         <location filename="../../lxqtnotification.cpp" line="152"/>
         <source>Notifications Fallback</source>
-        <translation type="unfinished"></translation>
+        <translation>Alternativ Påmindelse</translation>
     </message>
 </context>
 <context>
@@ -35,93 +155,93 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="112"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Dvale</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="119"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Slumre</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="126"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Genstart</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="133"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Luk ned</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="140"/>
         <source>Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Log ud</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="152"/>
         <source>LXQt Session Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Session Slumre</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="153"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du virkelig sætte din computer i slumretilstand?&lt;p&gt;Slumretilstand sætter computeren i energispare tilstand. Systemtilstanden mistes hvis strømmen forsvinder.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="162"/>
         <source>LXQt Session Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Session Dvale</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="163"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du virkelig sætte din computer i dvaletilstand?&lt;p&gt;Dvaletilstand sætter computeren i energispare tilstand. Systemtilstanden bibeholdes hvis strømmen forsvinder.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="172"/>
         <source>LXQt Session Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Session Genstart</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="173"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du virkelig genstarte din computer? Alt arbejde der ikke er blevet gemt, vil gå tabt...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="182"/>
         <source>LXQt Session Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Session Luk Ned</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="183"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du virkelig slukke din computer? Alt arbejde der ikke er blevet gemt, vil gå tabt...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="192"/>
         <source>LXQt Session Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Session Log Ud</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="193"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du virkelig logge ud? Alt arbejde der ikke er blevet gemt, vil gå tabt...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="201"/>
         <location filename="../../lxqtpowermanager.cpp" line="206"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt Strømstyringsfejl</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="201"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dvaletilstand mislykkedes.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="206"/>
         <source>Suspend failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Slumretilstand mislykkedes.</translation>
     </message>
 </context>
 <context>
@@ -129,7 +249,7 @@
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="51"/>
         <source>Lock Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Lås Skærm</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="77"/>
@@ -141,7 +261,7 @@
         <location filename="../../lxqtscreensaver.cpp" line="89"/>
         <location filename="../../lxqtscreensaver.cpp" line="96"/>
         <source>Screen Saver Activation Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl ved aktivering af pauseskærm</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="97"/>
@@ -149,19 +269,46 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
+        <translation type="vanished">Fejl ved start af screensaver. xdg-pauseskærm kan ikke starte på grund af nedbrud.</translation>
+    </message>
+    <message>
+        <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
+        <translation type="vanished">Fejl ved start af screensaver. xdg-pauseskærm er ikke korrekt installeret.</translation>
+    </message>
+    <message>
+        <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
+        <translation type="vanished">Fejl ved start af screensaver. xdg-pauseskærm kan ikke starte.</translation>
+    </message>
+    <message>
         <location filename="../../lxqtscreensaver.cpp" line="78"/>
         <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl ved start af screensaver. Syntaksfejl i argumenter til xdg-pauseskærm.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="84"/>
         <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl ved start af screensaver. Sørg for at du har xscreensaver installeret og kørende.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="90"/>
         <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl ved start af screensaver. Handling &apos;aktivere&apos; mislykkedes. Sørg for at du har xscreensaver installeret og kørende.</translation>
+    </message>
+    <message>
+        <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver=%1.</source>
+        <translation type="vanished">En fejl opstod ved start af pauseskærm. Ukendt fejl - udokumenteret returværdi sendt fra xdg-screensaver=%1.</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::ShortcutButton</name>
+    <message>
+        <source>Clear</source>
+        <translation type="obsolete">Rens</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="obsolete">Ingen</translation>
     </message>
 </context>
 <context>
@@ -170,19 +317,51 @@
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="91"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="141"/>
         <source>Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Strømstyrings fejl</translation>
     </message>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="92"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="142"/>
         <source>QDBusInterface is invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>QDBusInterface er ugyldig</translation>
     </message>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="106"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="156"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation type="unfinished"></translation>
+        <translation>Strømstyrings fejl (D-BUS kald)</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <source> About LXQt</source>
+        <translation type="vanished"> Om LXQt</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <comment>About dialog, Tab title </comment>
+        <translation type="vanished">Omkring</translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <comment>About dialog, Tab title</comment>
+        <translation type="vanished">Forfattere</translation>
+    </message>
+    <message>
+        <source>Thanks</source>
+        <comment>About dialog, Tab title</comment>
+        <translation type="vanished">Tak</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <comment>About dialog, Tab title</comment>
+        <translation type="vanished">Oversættelser</translation>
+    </message>
+    <message>
+        <source>Technical Info</source>
+        <comment>About dialog, Tab title</comment>
+        <translation type="vanished">Teknisk Information</translation>
     </message>
 </context>
 </TS>
