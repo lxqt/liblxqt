9fddff1f10141a2f2663944addb3628923418b47
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

--- liblxqt_id.ts
+++ liblxqt_id.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="id_ID">
+<TS version="2.1" language="id">
 <context>
     <name>ConfigDialog</name>
     <message>
         <location filename="../../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
 </context>
 <context>
@@ -14,12 +14,12 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="74"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Manajer Daya LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="74"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hibernasi gagal.</translation>
     </message>
 </context>
 <context>
@@ -27,7 +27,7 @@
     <message>
         <location filename="../../lxqtnotification.cpp" line="152"/>
         <source>Notifications Fallback</source>
-        <translation type="unfinished"></translation>
+        <translation>Fallback Notifikasi</translation>
     </message>
 </context>
 <context>
@@ -40,17 +40,17 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="124"/>
         <source>Suspend</source>
-        <translation>Suspend</translation>
+        <translation>Suspensi</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="131"/>
         <source>Reboot</source>
-        <translation>Reboot</translation>
+        <translation>Jalankan Ulang</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="138"/>
         <source>Shutdown</source>
-        <translation>Shutdown</translation>
+        <translation>Matikan</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="145"/>
@@ -60,68 +60,68 @@
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="157"/>
         <source>LXQt Session Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Suspensi Sesi LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="158"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Apakah Anda ingin mensuspensi komputer Anda?&lt;p&gt;Suspensi kompeter ke kondisi daya rendah. Kondisi sistem tidak disimpan jika daya hilang.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="167"/>
         <source>LXQt Session Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Hibernasi Sesi LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="168"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Apakah Anda ingin menghibernasi komputer Anda?&lt;p&gt;Hibernasi kompeter ke kondisi daya rendah. Kondisi sistem akan disimpan jika daya hilang.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="177"/>
         <source>LXQt Session Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Jalankan Ulang Sesi LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="178"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Apakah Anda ingin menjalankan ulang komputer? Semua pekerjaan yang belum disimpan akan hilang....</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="187"/>
         <source>LXQt Session Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Matikan Sesi LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="188"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Apakah Anda ingin mematikan komputer? Semua pekerjaan yang belum disimpan akan hilang....</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="197"/>
         <source>LXQt Session Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Keluar Sesi LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="198"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Apakah Anda ingin keluar? Semua pekerjaan yang belum disimpan akan hilang...</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="206"/>
         <location filename="../../lxqtpowermanager.cpp" line="211"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Manajer Daya LXQt</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="206"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hibernasi gagal.</translation>
     </message>
     <message>
         <location filename="../../lxqtpowermanager.cpp" line="211"/>
         <source>Suspend failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Suspensi gagal.</translation>
     </message>
 </context>
 <context>
@@ -129,39 +129,39 @@
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="236"/>
         <source>Lock Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunci Layar</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="156"/>
         <source>Screen Saver Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Screen Saver</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="162"/>
         <location filename="../../lxqtscreensaver.cpp" line="168"/>
         <location filename="../../lxqtscreensaver.cpp" line="175"/>
         <source>Screen Saver Activation Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Aktivasi Screen Saver</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="176"/>
         <source>An error occurred starting screensaver. Unknown error - undocumented return value from xdg-screensaver: %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Terjadi kesalahan saat menjalankan screensaver. Kesalahan tidak diketahui - nilai pengembalian tidak terdokumentasi dari xdg-screensaver: %1.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="157"/>
         <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
-        <translation type="unfinished"></translation>
+        <translation>Terjadi kesalahan saat menjalankan screensaver. Kesalahan sintaks di argumen xdg-screensaver.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="163"/>
         <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Terjadi kesalahan saat menjalankan screensaver. Pastikan xscreensaver telah terpasang dan berjalan.</translation>
     </message>
     <message>
         <location filename="../../lxqtscreensaver.cpp" line="169"/>
         <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Terjadi kesalahan saat menjalankan screensaver. Aksi &apos;activate&apos; gagal. Pastikan xscreensaver telah terpasang dan berjalan.</translation>
     </message>
 </context>
 <context>
@@ -170,19 +170,19 @@
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="87"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="137"/>
         <source>Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Manajer Daya</translation>
     </message>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="88"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="138"/>
         <source>QDBusInterface is invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>QDBusInterface tidak valid</translation>
     </message>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="102"/>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="152"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation type="unfinished"></translation>
+        <translation>Kesalahan Manajer Daya (D-BUS call)</translation>
     </message>
 </context>
 </TS>
