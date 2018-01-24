9ffe49e6bd40d530749be19e70c5e8a0b3ba1090
Luís Pereira <luis.artur.pereira@gmail.com>
2018-01-24 13:23:36 +0000

    Adds Qt Strings abbreviations
    It reduces the clutter. Ex: instead of writing:
    ```if (node.hasAttribute(QStringLiteral("http-contents-length")))```
    just write:
    ```if (node.hasAttribute(QSL("http-contents-length")))```

--- lxqtglobals.h
+++ lxqtglobals.h
@@ -36,4 +36,20 @@
 #define LXQT_API Q_DECL_IMPORT
 #endif
 
+#ifndef QL1S
+#define QL1S(x) QLatin1String(x)
+#endif
+
+#ifndef QL1C
+#define QL1C(x) QLatin1Char(x)
+#endif
+
+#ifndef QSL
+#define QSL(x) QStringLiteral(x)
+#endif
+
+#ifndef QBAL
+#define QBAL(x) QByteArrayLiteral(x)
+#endif
+
 #endif // _LXQT_GLOBALS_H_
