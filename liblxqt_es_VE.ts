9a0b1cbac31fde5c275a477f9d87368993b554de
Marcin Mikołajczak <me@m4sk.in>
2018-05-28 07:28:32 +0200

    Update translation files to current sources
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

--- liblxqt_es_VE.ts
+++ liblxqt_es_VE.ts
@@ -312,6 +312,14 @@
     </message>
 </context>
 <context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="49"/>
+        <source>Choose the page to be shown.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
         <location filename="../../lxqtpower/lxqtpowerproviders.cpp" line="87"/>
