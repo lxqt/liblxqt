88f8eab3f7f60a04a4b933461d595dd97e8b7ef3
Marcin Mikołajczak <me@m4sk.in>
2018-05-30 15:43:10 +0200

    Update Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

--- liblxqt_pl.ts
+++ liblxqt_pl.ts
@@ -316,7 +316,7 @@
     <message>
         <location filename="../../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="49"/>
         <source>Choose the page to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz stronę do wyświetlenia</translation>
     </message>
 </context>
 <context>
