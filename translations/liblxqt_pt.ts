31db5844e2092656653332ba2aa1c817d48afeb2
Sérgio Marques <smarquespt@gmail.com>
2018-08-21 21:30:36 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100,0% (33 of 33 strings)
    
    Translation: LXQt/LibLXQt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/liblxqt/pt/

--- translations/liblxqt_pt.ts
+++ translations/liblxqt_pt.ts
@@ -169,7 +169,7 @@
     <message>
         <location filename="../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="49"/>
         <source>Choose the page to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Escolha a página para mostrar.</translation>
     </message>
 </context>
 <context>
