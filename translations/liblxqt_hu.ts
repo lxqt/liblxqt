e274fa07671cefd21ab2447b9c6b1fac37ab57f4
- <petbottle@protonmail.com>
2018-09-01 10:34:27 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100,0% (33 of 33 strings)
    
    Translation: LXQt/LibLXQt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/liblxqt/hu/

--- translations/liblxqt_hu.ts
+++ translations/liblxqt_hu.ts
@@ -169,7 +169,7 @@
     <message>
         <location filename="../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="49"/>
         <source>Choose the page to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Megjelenítendő oldal kiválasztása.</translation>
     </message>
 </context>
 <context>
