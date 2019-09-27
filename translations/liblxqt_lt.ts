3d6a2bf79c8bf894f247c4a146ce0256192327e3
Moo <hazap@hotmail.com>
2019-09-27 11:03:06 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: LXQt/LibLXQt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/liblxqt/lt/

--- translations/liblxqt_lt.ts
+++ translations/liblxqt_lt.ts
@@ -129,12 +129,12 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="161"/>
         <source>Failed to run  &quot;%1&quot;. Ensure you have a locker/screensaver compatible with xdg-screensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko paleisti &quot;%1&quot;. Įsitikinkite, kad esate įdiegę ir paleidę su xdg-screensaver suderinamą užrakto įrankį/ekrano užsklandą.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="165"/>
         <source>Failed to run  &quot;%1&quot;. Ensure the specified locker/screensaver is installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko paleisti &quot;%1&quot;. Įsitikinkite, kad nurodytas užrakto įrankis/ekrano užsklanda yra įdiegta ir veikia.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="243"/>
