a840df0c9a869d74ef577a0c2df61b180500e299
Weblate <noreply@weblate.org>
2023-04-15 14:11:32 +0200

    weblate commits
    Co-authored-by: wallon <franckjl@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/liblxqt/fr/
    Translation: LXQt Desktop/liblxqt

--- translations/liblxqt_fr.ts
+++ translations/liblxqt_fr.ts
@@ -14,7 +14,7 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="76"/>
         <source>LXQt Power Manager Error</source>
-        <translation>Erreur du gestionnaire d&apos;alimentation de LXQt</translation>
+        <translation>Erreur du gestionnaire d’alimentation de LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="76"/>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="149"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation>Souhaitez-vous vraiment mettre en veille votre ordinateur ?&lt;p&gt;Votre ordinateur consommera un peu d&apos;énergie. L&apos;état du système ne sera pas préservé en cas de coupure de courant.</translation>
+        <translation>Souhaitez-vous vraiment mettre en veille votre ordinateur ?&lt;p&gt;Votre ordinateur consommera un peu d’énergie. L’état du système ne sera pas préservé en cas de coupure de courant.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="158"/>
@@ -75,7 +75,7 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="159"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation>Souhaitez-vous vraiment hiberner votre ordinateur ?&lt;p&gt;Votre ordinateur consommera un peu d&apos;énergie. L&apos;état du système sera préservé en cas de coupure de courant.</translation>
+        <translation>Souhaitez-vous vraiment hiberner votre ordinateur ?&lt;p&gt;Votre ordinateur consommera un peu d’énergie. L’état du système sera préservé en cas de coupure de courant.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="168"/>
@@ -111,7 +111,7 @@
         <location filename="../lxqtpowermanager.cpp" line="197"/>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <source>LXQt Power Manager Error</source>
-        <translation>Erreur du gestionnaire d&apos;alimentation de LXQt</translation>
+        <translation>Erreur du gestionnaire d’alimentation de LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="197"/>
@@ -129,22 +129,22 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="161"/>
         <source>Failed to run  &quot;%1&quot;. Ensure you have a locker/screensaver compatible with xdg-screensaver installed and running.</source>
-        <translation>Erreur lors du démarrage de  &quot;%1&quot;. Vérifiez que vous avez un écran de verouillage/veille compatible avec xdg-screensaver installé et démarré.</translation>
+        <translation>Erreur lors du démarrage de  « %1 ». Vérifiez que vous avez un écran de verouillage/veille compatible avec xdg-screensaver installé et démarré.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="165"/>
         <source>Failed to run  &quot;%1&quot;. Ensure the specified locker/screensaver is installed and running.</source>
-        <translation>Erreur lors du démarrage de &quot;%1&quot;. Vérifiez que le verouillage/écran de veille est installé et démarré.</translation>
+        <translation>Erreur lors du démarrage de « %1 ». Vérifiez que le verrouillage/écran de veille est installé et démarré.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="243"/>
         <source>Lock Screen</source>
-        <translation>Verrouiller l&apos;écran</translation>
+        <translation>Verrouiller l’écran</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="156"/>
         <source>Screen Saver Error</source>
-        <translation>Erreur de l&apos;économiseur d&apos;écran</translation>
+        <translation>Erreur de l’économiseur d’écran</translation>
     </message>
 </context>
 <context>
@@ -161,19 +161,19 @@
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="87"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="137"/>
         <source>Power Manager Error</source>
-        <translation>Erreur du gestionnaire d&apos;alimentation de LXQt</translation>
+        <translation>Erreur du gestionnaire d’alimentation de LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="88"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="138"/>
         <source>QDBusInterface is invalid</source>
-        <translation>L&apos;interface QDBus est invalide</translation>
+        <translation>L’interface QDBus est invalide</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="102"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="152"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation>Erreur du gestionnaire d&apos;alimentation de LXQt (appel de D-Bus)</translation>
+        <translation>Erreur du gestionnaire d’alimentation de LXQt (appel de D-Bus)</translation>
     </message>
 </context>
 </TS>
