dbba38d813188c924939baf77886653eddea9b6f
Weblate <noreply@weblate.org>
2024-11-28 21:36:53 +0100

    weblate commits
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/liblxqt/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/liblxqt/ko/
    Translation: LXQt Desktop/liblxqt

--- translations/liblxqt_kab.ts
+++ translations/liblxqt_kab.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko">
+<TS version="2.1" language="kab">
 <context>
     <name>ConfigDialog</name>
     <message>
         <location filename="../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation>대화창</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -14,12 +14,12 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="76"/>
         <source>LXQt Power Manager Error</source>
-        <translation>LXQt 전원 관리자 오류</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="76"/>
         <source>Hibernate failed.</source>
-        <translation>최대 절전모드에 실패했습니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -27,7 +27,7 @@
     <message>
         <location filename="../lxqtnotification.cpp" line="161"/>
         <source>Notifications Fallback</source>
-        <translation>알림 대체</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -35,93 +35,93 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="108"/>
         <source>Hibernate</source>
-        <translation>최대 절전</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="115"/>
         <source>Suspend</source>
-        <translation>절전</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="122"/>
         <source>Reboot</source>
-        <translation>재시작</translation>
+        <translation>Ales asenker</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="129"/>
         <source>Shutdown</source>
-        <translation>시스템 종료</translation>
+        <translation>Sexsi</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="136"/>
         <source>Logout</source>
-        <translation>로그아웃</translation>
+        <translation>Senser tuqqna</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="148"/>
         <source>LXQt Session Suspend</source>
-        <translation>LXQt 세션 절전</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="149"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation>컴퓨터를 절전모드로 전환하시겠습니까??&lt;p&gt;컴퓨터를 저전력 상태인 절전모드로 전환합니다. 전원이 꺼지면 시스템 상태가 유지되지 않습니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="158"/>
         <source>LXQt Session Hibernate</source>
-        <translation>LXQt 세션 최대 절전</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="159"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation>컴퓨터를 최대 절전모드로 전환하시겠습니까?&lt;p&gt;컴퓨터를 저전력 상태인 최대 절전모드로 전환합니다. 전원이 꺼지면 시스템 상태가 유지되지 않습니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="168"/>
         <source>LXQt Session Reboot</source>
-        <translation>LXQt 세션 재시작</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="169"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation>컴퓨터를 정말로 다시 시작하시겠습니까? 저장하지 않은 모든 작업이 손실됩니다...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="178"/>
         <source>LXQt Session Shutdown</source>
-        <translation>LXQt 세션 시스템 종료</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="179"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation>정말로 컴퓨터를 끄시겠습니까? 저장하지 않은 모든 작업이 손실됩니다...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="188"/>
         <source>LXQt Session Logout</source>
-        <translation>LXQt 세션 로그아웃</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="189"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation>로그아웃 하시겠습니다? 저장하지 않은 모든 작업이 손실됩니다...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="197"/>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <source>LXQt Power Manager Error</source>
-        <translation>LXQt 전원 관리자 오류</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="197"/>
         <source>Hibernate failed.</source>
-        <translation>최대 절전모드에 실패했습니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <source>Suspend failed.</source>
-        <translation>절전모드에 실패했습니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -129,22 +129,22 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="161"/>
         <source>Failed to run  &quot;%1&quot;. Ensure you have a locker/screensaver compatible with xdg-screensaver installed and running.</source>
-        <translation>&quot;%1&quot;을(를) 실행하지 못했습니다. xdg-screensaver와 호환되는 잠금도구/화면 보호기가 설치되어 실행 중인지 확인하세요.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="165"/>
         <source>Failed to run  &quot;%1&quot;. Ensure the specified locker/screensaver is installed and running.</source>
-        <translation>&quot;%1&quot;을(를) 실행하지 못했습니다. 지정된 잠금도구/화면 보호기가 설치되어 실행 중인지 확인하세요.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="247"/>
         <source>Lock Screen</source>
-        <translation>화면 잠금</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="156"/>
         <source>Screen Saver Error</source>
-        <translation>화면 보호기 오류</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -152,7 +152,7 @@
     <message>
         <location filename="../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="47"/>
         <source>Choose the page to be shown.</source>
-        <translation>표시할 페이지를 선택합니다.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -161,19 +161,19 @@
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="87"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="137"/>
         <source>Power Manager Error</source>
-        <translation>전원 관리자 오류</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="88"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="138"/>
         <source>QDBusInterface is invalid</source>
-        <translation>QDBusInterface에 이상이 있습니다</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="102"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="152"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation>전원 관리자 오류(D-BUS 호출)</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
