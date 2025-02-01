35bdb43a95792cc898ffc09d137ce71554efda67
isf63 <121320947+isf63@users.noreply.github.com>
2025-02-01 09:06:53 -0500

    Fix typos to comments and CHANGELOG (#359)
    * Fix typos (mainly to comments/CHANGELOG)
    
    * Revert changes to code identifiers

--- lxqtapplication.h
+++ lxqtapplication.h
@@ -56,7 +56,7 @@ public:
     /*! Construct a LXQt application object.
      * \param argc standard argc as in QApplication
      * \param argv standard argv as in QApplication
-     * \param handleQuitSignals flag if signals SIGINT, SIGTERM, SIGHUP should be handled internaly (\sa quit() application)
+     * \param handleQuitSignals flag if signals SIGINT, SIGTERM, SIGHUP should be handled internally (\sa quit() application)
      */
     Application(int &argc, char **argv, bool handleQuitSignals);
     ~Application() override {}
