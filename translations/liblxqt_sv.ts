56e497cee59d13948fa2b41c93bd8b9076217a5c
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-06-20 16:15:23 +0200

    weblate commits (#363)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/liblxqt/sv/
    Translation: LXQt Desktop/liblxqt
    
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>

---
+++ translations/liblxqt_sv.ts
@@ -0,0 +1,179 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../configdialog/lxqtconfigdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::MessageBox</name>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="76"/>
+        <source>LXQt Power Manager Error</source>
+        <translation>Fel från LXQt strömhantering</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="76"/>
+        <source>Hibernate failed.</source>
+        <translation>Kunde inte gå in i viloläge.</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::NotificationPrivate</name>
+    <message>
+        <location filename="../lxqtnotification.cpp" line="161"/>
+        <source>Notifications Fallback</source>
+        <translation>Reservlösning för aviseringar</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::PowerManager</name>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="108"/>
+        <source>Hibernate</source>
+        <translation>Viloläge</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="115"/>
+        <source>Suspend</source>
+        <translation>Vänteläge</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="122"/>
+        <source>Reboot</source>
+        <translation>Starta om</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="129"/>
+        <source>Shutdown</source>
+        <translation>Stäng av</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="136"/>
+        <source>Logout</source>
+        <translation>Logga ut</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="148"/>
+        <source>LXQt Session Suspend</source>
+        <translation>LXQt vänteläge</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="149"/>
+        <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
+        <translation>Vill du verkligen låta datorn gå in i vänteläge?&lt;p&gt;Datorn kommer att försättas i lågenergiläge. Systemtillståndet går förlorat om strömförsörjningen stängs av.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="158"/>
+        <source>LXQt Session Hibernate</source>
+        <translation>LXQt viloläge</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="159"/>
+        <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
+        <translation>Vill du verkligen låta datorn gå in i viloläge?&lt;p&gt;Datorn kommer att försättas i lågenergiläge. Systemtillståndet går förlorat om strömförsörjningen stängs av.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="168"/>
+        <source>LXQt Session Reboot</source>
+        <translation>Starta om LXQt-session</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="169"/>
+        <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
+        <translation>Vill du verkligen starta om datorn? Allt osparat arbete kommer förloras...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="178"/>
+        <source>LXQt Session Shutdown</source>
+        <translation>Stäng av LXQt-session</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="179"/>
+        <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
+        <translation>Vill du verkligen stänga av datorn? Allt osparat arbete kommer förloras...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="188"/>
+        <source>LXQt Session Logout</source>
+        <translation>Logga ut från LXQt-session</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="189"/>
+        <source>Do you want to really logout? All unsaved work will be lost...</source>
+        <translation>Vill du verkligen logga ut? Allt osparat arbete kommer förloras...</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="197"/>
+        <location filename="../lxqtpowermanager.cpp" line="202"/>
+        <source>LXQt Power Manager Error</source>
+        <translation>Fel från LXQt strömhantering</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="197"/>
+        <source>Hibernate failed.</source>
+        <translation>Kunde inte gå in i viloläge.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpowermanager.cpp" line="202"/>
+        <source>Suspend failed.</source>
+        <translation>Kunde inte gå in i vänteläge.</translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::ScreenSaver</name>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="161"/>
+        <source>Failed to run  &quot;%1&quot;. Ensure you have a locker/screensaver compatible with xdg-screensaver installed and running.</source>
+        <translation>Misslyckades med att köra &quot;%1&quot;. Kontrollera att du har skärmlås eller skärmsläckare installerat som är kompatibel med xdg-skärmsläckaren.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="165"/>
+        <source>Failed to run  &quot;%1&quot;. Ensure the specified locker/screensaver is installed and running.</source>
+        <translation>Kunde inte köra  &quot;%1&quot;. Kontrollera att det finns skärmlås eller skärmsläckare installerat.</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="247"/>
+        <source>Lock Screen</source>
+        <translation>Lås skärm</translation>
+    </message>
+    <message>
+        <location filename="../lxqtscreensaver.cpp" line="156"/>
+        <source>Screen Saver Error</source>
+        <translation>Fel från skärmsläckare</translation>
+    </message>
+</context>
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="47"/>
+        <source>Choose the page to be shown.</source>
+        <translation>Välj vilken sida som ska visas.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="87"/>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="137"/>
+        <source>Power Manager Error</source>
+        <translation>Fel från strömhanterare</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="88"/>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="138"/>
+        <source>QDBusInterface is invalid</source>
+        <translation>QDBusInterface är ogiltigt</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="102"/>
+        <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="152"/>
+        <source>Power Manager Error (D-BUS call)</source>
+        <translation>Fel från strömhanterare (D-BUS anrop)</translation>
+    </message>
+</context>
+</TS>
