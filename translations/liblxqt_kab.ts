1ee8ce94bdaf1a04ea0289f80174671c4c1c0a88
Weblate <noreply@weblate.org>
2026-01-06 19:28:09 +0100

    weblate commits
    Co-authored-by: Madɣis <massiin@proton.me>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/liblxqt/kab/
    Translation: LXQt Desktop/liblxqt

--- translations/liblxqt_kab.ts
+++ translations/liblxqt_kab.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../configdialog/lxqtconfigdialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Tanaka n udiwenni</translation>
     </message>
 </context>
 <context>
@@ -14,12 +14,12 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="76"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuccḍa n umsefrak n tnezmart LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="76"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Asgen ur yeddi ara.</translation>
     </message>
 </context>
 <context>
@@ -27,7 +27,7 @@
     <message>
         <location filename="../lxqtnotification.cpp" line="161"/>
         <source>Notifications Fallback</source>
-        <translation type="unfinished"></translation>
+        <translation>Ilɣa n usellek</translation>
     </message>
 </context>
 <context>
@@ -35,12 +35,12 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="108"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Sgen</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="115"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Ḥbes di leɛḍil</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="122"/>
@@ -60,68 +60,68 @@
     <message>
         <location filename="../lxqtpowermanager.cpp" line="148"/>
         <source>LXQt Session Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Aseḥbes di leɛḍil n tɣimit LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="149"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>D tidet tebɣiḍ ad tḥebseḍ di leɛḍil aselkim-ik·im?&lt;p&gt;Aseḥbes di leɛḍil n uselkim ɣer waddad n tezmert yudren. Addad n unagraw ur yettwaḥrez ara ma tella tazmert tegzem.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="158"/>
         <source>LXQt Session Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Asgen n tɣimit LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="159"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>D tidet tebɣiḍ asgen n uselkim-ik·im?&lt;p&gt; Asgen n uselkim ɣer waddad n tezmert yudren. Addad n unagraw yettwaḥrez ma tella tezmart tegzem.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="168"/>
         <source>LXQt Session Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ales usenker n tɣimit LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="169"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>D tidet tebɣiḍ ad talseḍ asenker n uselkim-ik·im? Amahil akk ur yettwaskelsen ara ad iṛuḥ…</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="178"/>
         <source>LXQt Session Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Sexsi tiɣimit LXQ</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="179"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>Tebɣiḍ s tidet ad tessexsiḍ aselkim-ik·im? Akk amahil ur yettwaskelsen ara ad iṛuḥ...</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="188"/>
         <source>LXQt Session Logout</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuffɣa n tiɣimit LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="189"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
-        <translation type="unfinished"></translation>
+        <translation>D tidet tebɣiḍ ad teffɣeḍ? Amahil akk ur yettwaskelsen ara ad iṛuḥ..</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="197"/>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <source>LXQt Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuccḍa n umsefrak n tezmert LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="197"/>
         <source>Hibernate failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Asgen ur yeddi ara.</translation>
     </message>
     <message>
         <location filename="../lxqtpowermanager.cpp" line="202"/>
         <source>Suspend failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuccḍa deg useḥbes di leɛḍil.</translation>
     </message>
 </context>
 <context>
@@ -129,22 +129,22 @@
     <message>
         <location filename="../lxqtscreensaver.cpp" line="161"/>
         <source>Failed to run  &quot;%1&quot;. Ensure you have a locker/screensaver compatible with xdg-screensaver installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aselkem n &quot;%1&quot; ur yeddi ara. Wali ma tesɛiḍ asekkeṛ/Agdil n uḥraz yemṣada d xdg-screensaver yerna ittusbedd u iteddu.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="165"/>
         <source>Failed to run  &quot;%1&quot;. Ensure the specified locker/screensaver is installed and running.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aselkem n &quot;%1&quot; ur yeddi ara. Wali ma yella usekkeṛ i d-ittunefken/Agdil n uḥraz yettwasbedd yernu iteddu.</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="247"/>
         <source>Lock Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Sekkeṛ agdil</translation>
     </message>
     <message>
         <location filename="../lxqtscreensaver.cpp" line="156"/>
         <source>Screen Saver Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuccḍa deg uḥraz n ugdil</translation>
     </message>
 </context>
 <context>
@@ -152,7 +152,7 @@
     <message>
         <location filename="../configdialog/lxqtconfigdialogcmdlineoptions.cpp" line="47"/>
         <source>Choose the page to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fren asebtar ara yettwabeqqeḍen.</translation>
     </message>
 </context>
 <context>
@@ -161,19 +161,19 @@
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="87"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="137"/>
         <source>Power Manager Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuccḍa n umsefrak n tezmart</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="88"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="138"/>
         <source>QDBusInterface is invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>QDBusInterface mačči d ameɣtu</translation>
     </message>
     <message>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="102"/>
         <location filename="../lxqtpower/lxqtpowerproviders.cpp" line="152"/>
         <source>Power Manager Error (D-BUS call)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tuccḍa n umsefrak n tezmart (tiɣri D-BUS)</translation>
     </message>
 </context>
 </TS>
